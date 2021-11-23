bbedab829253a7d88a946334d20746c005843ec8
Michael Smith <m.t.smith@sheffield.ac.uk>
2021-11-23 13:03:00 +0000

    added empty methods

--- advectionGP/__init__.py
+++ advectionGP/__init__.py
@@ -3,9 +3,9 @@ import numpy as np
 class Kernel():
     def __init__(self):
         assert False, "Not implemented" #TODO Turn into an exception
-    def generate_features(self,N_D,N_feat):
+    def generateFeatures(self,N_D,N_feat):
         assert False, "Not implemented" #TODO Turn into an exception
-    def compute_phi(self):
+    def computePhi(self):
         assert False, "Not implemented" #TODO Turn into an exception
         
 class EQ(Kernel):
@@ -18,10 +18,10 @@ class EQ(Kernel):
         """
         self.l2 = l2
         self.sigma2 = sigma2
-        self.W = None #need to be set by calling generate_features.
+        self.W = None #need to be set by calling generateFeatures.
         self.b = None 
                 
-    def generate_features(self,N_D,N_feat):
+    def generateFeatures(self,N_D,N_feat):
         """
         Create a random basis for the kernel.
         Arguments:
@@ -33,34 +33,69 @@ class EQ(Kernel):
         self.N_D = N_D
         self.N_feat = N_feat
         
-    def compute_phi(self,coords):
+    def computePhi(self,coords):
         """
-        Compute phi (posterior matrix of basis vectors)
+        ComputePhi (posterior matrix of basis vectors)
         Arguments:
             coords = an N by N_D array of locations
         """
-        assert self.W is not None, "Need to call generate_features before computing phi."
+        assert self.W is not None, "Need to call generateFeatures before computing phi."
         
         norm = 1./np.sqrt(self.N_feat)
         c=np.sqrt(2.0)/(self.l2)
-        phi_matrix = norm*np.sqrt(2*self.sigma2)*np.cos(c*(self.W@coords)+ self.b[:,None])
-        phi_matrix = phi_matrix.reshape(N_feat,Nt,Nx,Ny)
-        return phi_matrix
+        phiMatrix = norm*np.sqrt(2*self.sigma2)*np.cos(c*(self.W@coords)+ self.b[:,None])
+        phiMatrix = phiMatrix.reshape(N_feat,Nt,Nx,Ny)
+        return phiMatrix
+       
+class SensorModel():
+    def __init__(self):
+        """Builds H"""
+        assert False, "Not implemented" #TODO Turn into an exception
+
         
+    def getHs(self):
+        assert False, "Not implemented" #TODO Turn into an exception
+    
+class FixedSensorModel(SensorModel):
+    def __init__(self,obsLocations,spatialAveraging):
+        """Return a self.resolution array describing how the concentration is added up for an observation in x.
+        Uses self.spatial_averaging to extend the part of the domain that is being observed.
+        
+        Parameters:
+            x == a 4 element vector, time_start, time_end, x, y
+            
+        The getHs method returns a model.resolution sized numpy array
+        """
+        self.obsLocs = obsLocations
+        self.spatialAveraging = spatialAveraging
+        #TO DO
+       
+    def getHs(self,model):
+        halfGridTile = np.array([0,self.spatialAveraging/2,self.spatialAveraging/2])
+        startOfHs = model.getGridCoord(self.obsLocs[:,[0,2,3]]-halfGridTile)
+        endOfHs = model.getGridCoord(self.obsLocs[:,[1,2,3]]+halfGridTile)
+        for start,end,tlength in zip(startOfHs,endOfHs,self.obsLocs[:,1]-self.obsLocs[:,0]):
+            h = np.zeros(model.resolution)
+            h[start[0]:end[0],start[1]:end[1],start[2]:end[2]] = self.spatialAveraging**2 * tlength
+            print(start[0],end[0],start[1],end[1],start[2],end[2])
+            yield h
+        
+#X = an N by 4 matrix of sensor times and locations [time_start, time_end, x, y]
+#y = an N long vector of the measurements associated with the sensors.
+         
 class Model():
-    def __init__(self,X,y,boundary,resolution,kernel,noise_std,N_feat=25,spatial_averaging=1.0):
+    def __init__(self,boundary,resolution,kernel,noise_std,sensormodel,N_feat=25,spatial_averaging=1.0):
         """
         The Advection Diffusion Model.
         
         At the moment we assume a 3d grid [time, x, y].
         
         Parameters:
-            X = an N by 4 matrix of sensor times and locations [time_start, time_end, x, y]
-            y = an N long vector of the measurements associated with the sensors.
             boundary = a two element tuple of the corners of the grid. e.g. ([0,0,0],[10,10,10])        
             resolution = a list of the grid size in each dimension. e.g. [10,20,20]
             kernel = the kernel to use
             noise_std = the noise standard deviation
+            sensormodel = an instatiation of a SensorModel class that implements the getHs method.
             N_feat = number of fourier features
             spatial_averaging = how big the volume the sensor measures (default 0.001).
         """
@@ -71,47 +106,70 @@ class Model():
         assert self.N_D==len(boundary[0]), "The dimensions in the boundary don't match those in the grid resolution"
         assert self.N_D==len(boundary[1]), "The dimensions in the boundary don't match those in the grid resolution"
 
-        self.spatial_averaging = spatial_averaging
-        self.X = X
-        self.y = y
-        self.boundary = boundary
-        self.resolution = resolution
+        #self.spatial_averaging = spatial_averaging
+        #self.X = X
+        #self.y = y
+        self.boundary = [np.array(boundary[0]),np.array(boundary[1])]
+        self.resolution = np.array(resolution)
         self.noise_std = noise_std
+        self.sensormodel = sensormodel
       
 
-        assert self.X.shape[1]==4, "The X input matrix should be Nx4."
-        assert self.y.shape[0]==self.X.shape[0], "The length of X should equal the length of y"
+        #assert self.X.shape[1]==4, "The X input matrix should be Nx4."
+        #assert self.y.shape[0]==self.X.shape[0], "The length of X should equal the length of y"
         self.kernel = kernel
-        self.kernel.generate_features(self.N_D,N_feat) 
+        self.kernel.generateFeatures(self.N_D,N_feat) 
         self.N_feat = N_feat
         
-    def compute_H(self,x):
-        """Return a self.resolution array describing how the concentration is added up for an observation in x.
-        Uses self.spatial_averaging to extend the part of the domain that is being observed.
+    def getGridCoord(self,realPos):
+        """
+        Gets the location on the mesh for a real position
+        """
+        return (self.resolution*(realPos - self.boundary[0])/(self.boundary[1]-self.boundary[0])).astype(int)
         
-        Parameters:
-            x == a 4 element vector, time_start, time_end, x, y
-            
-        Returns:
-            H = a self.resolution sized numpy array
+    def computeConcentration(self,source):
+        #TODO
+        #concentration
+        self.concentration = None #TODO
+      
+    def computeObservations(self, sensorModel):
+        """       
+        Using the forward model
+        ##todo        
         """
+        #use self.concentration
+        for h in self.sensorModel.getHs():
+            pass
+            #run forward model and get H
+        self.ySimulated = None
         
-    def compute_TODONEEDNAME(): #TODO
-        """compute the backward advection-diffusion PDE with finite difference method."""
-        pass
-       
-    def compute_v(self):
+    def computeSourceFromPhi(self):
         """
-        Computes v.
-        Returns
-            v = an N by ? ... 
         """
-        for x in X:
-            H = self.compute_H(x)
-            #TODO: URGENT: REALLY NEED CLEARER METHOD NAMES AND VARIABLE NAMES...
-            y_Conv_Diff_back=-Convection_Diffusion_back(H)
-            #TODO ...
-            y_bvv.append(y_Conv_Diff_back*(xmax/Nx)*(ymax/Ny)*(tmax/Nt))
-
-        return y_bvv#TODO...
-     
+        #uses self.phi and self.z to compute self.sources
+        self.sources = None
+        
+    def computePhi(self):
+        """
+        """
+        #use self.W and self.b to find self.phi
+        self.phi = None
+        
+    def computeAdjoint(self,sensorModel):
+        """
+        """
+        #for each feature computes the adjoint array. v
+        self.v = None
+        
+    def computeModelRegressors(self):
+        """
+        """
+        #phi * v, --> scale
+        self.X = None
+    
+    def computeZDistribution(self,y):
+        """
+        """
+        #uses self.X and observations y.
+        self.zMean = None
+        self.zCov = None
