fixing the broken spatial averaging that I just introduced

advectionGP/sensors.py
@@ -45,8 +45,8 @@ class FixedSensorModel(SensorModel):
         dt,dx,dy,dx2,dy2,Nt,Nx,Ny = model.getGridStepSize()
         for start,end,tlength in zip(startOfHs,endOfHs,self.obsLocs[:,1]-self.obsLocs[:,0]):
             h = np.zeros(model.resolution)
-            h[start[0]:end[0],start[1]:end[1],start[2]:end[2]] = 1#/(self.spatialAveraging**2 * tlength)
-            h /= (np.sum(h)*dt*dx*dy)
+            h[start[0]:end[0],start[1]:end[1],start[2]:end[2]] = 1/(self.spatialAveraging**2 * tlength)
+            #h /= (np.sum(h)*dt*dx*dy)
             #print(start[0],end[0],start[1],end[1],start[2],end[2])
             yield h
             