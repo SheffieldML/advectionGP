c93cc911624b0ee2e9e01ee338c6ab2e20ef30ea
cwlanyon <chris@fracture-systems.co.uk>
2022-07-31 12:28:28 +0100

    Further updates to GP comparison

--- advectionGP/models.py
+++ advectionGP/models.py
@@ -722,5 +722,180 @@ class AdjointSecondOrderODEModel(SecondOrderODEModel):
         return meanZ, covZ   
     
     
-    
+class ShiftOperatorModel():
+    def __init__(self,boundary,resolution,kernel,noiseSD,sensormodel,N_feat=25,spatial_averaging=1.0):
+        """
+        The Advection Diffusion Model.
+        
+        At the moment we assume a 3d grid [time, x, y].
+        
+        Parameters:
+            boundary = a two element tuple of the corners of the grid. e.g. ([0,0,0],[10,10,10])        
+            resolution = a list of the grid size in each dimension. e.g. [10,20,20]
+            kernel = the kernel to use
+            noiseSD = the noise standard deviation
+            sensormodel = an instatiation of a SensorModel class that implements the getHs method.
+            N_feat = number of fourier features
+            spatial_averaging = how big the volume the sensor measures (default 0.001).
+            windmodel = an instance of a Wind class (to build u using)
+            k_0 = diffusion constant
+            
+        When using real data (e.g. wind etc) we assume the units are:
+         - km
+         - hours
+         - km/h
+         - pollution can be anything, we assume at the moment PM2.5 in ug/m^3.
+        
+        """
+        #TODO URGENT: The spatial averaging doesn't make sense!
+        #TODO The wind speed and diffusion might need to be vectors
+        
+        self.N_D = len(resolution)
+        assert self.N_D==3, "Currently AdvectionDiffusionModel only supports a 3d grid: T,X,Y. Check your resolution parameter."
+        assert self.N_D==len(boundary[0]), "The dimensions in the boundary don't match those in the grid resolution"
+        assert self.N_D==len(boundary[1]), "The dimensions in the boundary don't match those in the grid resolution"
+
+
+        self.boundary = [np.array(boundary[0]),np.array(boundary[1])]
+        self.resolution = np.array(resolution)
+        self.noiseSD = noiseSD
+        self.sensormodel = sensormodel
+        
+        
+        #coords is a D x (Nt,Nx,Ny) array of locations of the grid vertices.
+        #TODO Maybe write more neatly...
+        tt=np.linspace(self.boundary[0][0],self.boundary[1][0],self.resolution[0])
+        xx=np.linspace(self.boundary[0][1],self.boundary[1][1],self.resolution[1])
+        yy=np.linspace(self.boundary[0][2],self.boundary[1][2],self.resolution[2])
+        self.coords=np.asarray(np.meshgrid(tt,xx,yy,indexing='ij'))
+        #self.coords=coords.reshape(self.N_D,self.resolution[0]*self.resolution[1]*self.resolution[2])
+      
+        #Compute some variables useful for PDEs
+
+        
+
+        #assert self.X.shape[1]==4, "The X input matrix should be Nx4."
+        #assert self.y.shape[0]==self.X.shape[0], "The length of X should equal the length of y"
+        self.kernel = kernel
+        self.kernel.generateFeatures(self.N_D,N_feat) 
+        self.N_feat = N_feat
+        
+
+
+        dt,dx,dy,dx2,dy2,Nt,Nx,Ny = self.getGridStepSize()
+
+    def getGridStepSize(self):
+        """
+        Calculates useful scalars for the PDE model
+        outputs:
+            dt: time grid size
+            dx: x direction grid size
+            dy: y direction grid size
+            dx2 = dx**2
+            dy2 = dy**2
+            Nt: Number of evaluation points in time
+            Nx: Number of evaluation points in x axis
+            Ny: Number of evaluation points in y axis
+        """
+        dt=(self.boundary[1][0]-self.boundary[0][0])/self.resolution[0]
+        dx=(self.boundary[1][1]-self.boundary[0][1])/self.resolution[1]
+        dy=(self.boundary[1][2]-self.boundary[0][2])/self.resolution[2]
+        dx2=dx*dx
+        dy2=dy*dy
+        Nt=self.resolution[0]
+        Nx=self.resolution[1]
+        Ny=self.resolution[2]
+        return dt,dx,dy,dx2,dy2,Nt,Nx,Ny
+        
+    def getGridCoord(self,realPos):
+        """
+        Gets the location on the mesh for a real position
+        I.e. Given a valume in m getGridCoord returns the location on the grid
+        
+        todo: assertion for out of bounds value
+        
+        Q: why is this floor and not round?
+        """
+        return np.floor(self.resolution*(realPos - self.boundary[0])/(self.boundary[1]-self.boundary[0])).astype(int)
+        #return np.round(self.resolution*(realPos - self.boundary[0])/(self.boundary[1]-self.boundary[0])).astype(int)
+   
+      
+    def computeObservations(self,addNoise=False):
+        """       
+        Generates test observations by calculating the inner product of the filter function from the senor model and a given self.conc.
+        Arguments:
+            addNoise: if addNoise is True then random noise is added to the observations from a normal distribution with mean 0 and standard deviation noiseSD. 
+        """
+        
+        #TODO Need to write a unit test
+        #use self.conc
+        dt,dx,dy,dx2,dy2,Nt,Nx,Ny = self.getGridStepSize()
+        obs = np.zeros(len(self.sensormodel.obsLocs))
+        for it,h in enumerate(self.sensormodel.getHs(self)):
+            #TODO Make this faster - replacing the sums with matrix operations
+            obs[it]=np.sum(h*self.conc)*dt*dx*dy
+            if addNoise:
+                obs[it]+=np.random.normal(0.0,self.noiseSD,1)  
+        self.ySimulated = obs
+        return obs
+        
+     
+        
+    def computeSourceFromPhi(self,z,coords=None):
+        """
+        uses getPhi from the kernel and a given z vector to generate a source function     
+        set coords to a matrix: (3 x Grid Resolution), e.g. (3, 300, 80, 80)
+                e.g. coords=np.asarray(np.meshgrid(tt,xx,yy,indexing='ij'))
+        
+        """
+        if coords is None: coords = self.coords
+        resolution = np.array(coords.shape[1:])
+        self.source = np.zeros(resolution) 
+        
+        for i,phi in enumerate(self.kernel.getPhi(self.coords)):
+            self.source += phi*z[i]
+        
+        return self.source
+                
+    def computeConcentration(self,source,enforce_nonnegative=False):
+        """
+        Computes concentrations.
+        Arguments:
+         source == forcing function (shape: Nt x Nx x Ny). Can either be generated by ... or determine manually.
+         enforce_nonnegative = default False,. Setting to true will force concentration to be non-negative each iteration.
+        returns array of concentrations (shape: Nt x Nx x Ny), given source. (also saved it in self.concentration)
+        """
+        #source = self.source
+        
+        #get the grid step sizes, their squares and the size of the grid
+        dt,dx,dy,dx2,dy2,Nt,Nx,Ny = self.getGridStepSize()
+        
+        c=np.zeros(((self.resolution)))
+        
+        
+
+        for i in range(5,Nt):
+            # Internal Calc
+            c[i,:,:]=(source[i-5,:,:])
+            if enforce_nonnegative: c[c<0]=0
+        concentration = c 
+        
+        self.conc = concentration
+        return c      
+
+class AdjointShiftOperatorModel(ShiftOperatorModel,AdjointAdvectionDiffusionModel):
+    def computeAdjoint(self,H):
+        """
+        Runs the backward PDE (adjoint problem)
+        Gets called for an observation instance (H).
+        (v is the result of the adjoint operation)
+        """
+        dt,dx,dy,dx2,dy2,Nt,Nx,Ny = self.getGridStepSize()
+
+        v=np.zeros(((Nt,Nx,Ny)))
+     
+        for i in range(1,Nt-5): #TODO might be better to rewrite as range(Nt-1,1,-1)...
+    #Internal calculation (not on the boundary)
+            v[i,:,:]= H[i+5,:,:]
+        return v
 
